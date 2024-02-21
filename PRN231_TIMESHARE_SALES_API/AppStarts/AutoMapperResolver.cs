826dfd0654ae865155d91690a69bf172036f0c98
nea287 <thonguyenngockieu@gmail.com>
2024-02-21 23:55:27 +0700

    update/ auto mapper

--- PRN231_TIMESHARE_SALES_API/AppStarts/AutoMapperResolver.cs
+++ PRN231_TIMESHARE_SALES_API/AppStarts/AutoMapperResolver.cs
@@ -34,30 +34,9 @@ namespace PRN231_TIMESHARE_SALES_API.AppStarts
             #endregion
 
             #region StaffOfProject
-            //CreateMap<Account, AccountViewModel>()
-            //    .ForMember(x => x.Projects, dest => dest.MapFrom(o => o.Projects));
-
-            CreateMap<StaffOfProjectFilter, AccountViewModel>().ForMember(x => x.Projects, dest => dest.MapFrom(opt => new Project() { 
-            ProjectCode = opt.ProjectCode,
-            ProjectName = opt.ProjectName,
-            ProjectId = opt.ProjectId.Value,
-            RegistrationEndDate = opt.RegistrationEndDate.Value,
-            EndDate = opt.EndDate.Value,
-            StartDate = opt.StartDate.Value,
-            RegistrationOpeningDate = opt.RegistrationOpeningDate.Value,
-            PriorityType = opt.PriorityType,
-            Status = opt.Status.Value,
-            TotalSlot = opt.TotalSlot.Value,
-            Departments = new List<Department>(),
-            staff = new List<Account>()
-            })).ReverseMap();
-
-
-            //CreateMap<AccountViewModel, StaffOfProjectFilter>()
-            //    .ForMember(x => x.ProjectCode, dest => dest.MapFrom(opt => opt.Projects.First().ProjectCode));
-            
-            //CreateMap<AccountViewModel, StaffOfProjectFilter>()
-            //    .ForMember(x => x.ProjectName, dest => dest.MapFrom(opt => opt.Projects.First().ProjectName));
+            CreateMap<StaffOfProject, StaffOfProjectRequestModel>().ReverseMap();
+            CreateMap<StaffOfProject, StaffOfProjectsViewModel>().ReverseMap();
+            CreateMap<StaffOfProjectRequestModel, StaffOfProjectsViewModel>().ReverseMap();
 
             #endregion
 
