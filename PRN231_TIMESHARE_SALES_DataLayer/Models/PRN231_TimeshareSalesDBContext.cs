8a5b64b67293591250e5f9db9e1bba111699619c
nea287 <thonguyenngockieu@gmail.com>
2024-03-04 15:53:00 +0700

    fix/db context

--- PRN231_TIMESHARE_SALES_DataLayer/Models/PRN231_TimeshareSalesDBContext.cs
+++ PRN231_TIMESHARE_SALES_DataLayer/Models/PRN231_TimeshareSalesDBContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace PRN231_TIMESHARE_SALES_DataLayer.Models
 {
@@ -34,9 +35,18 @@ namespace PRN231_TIMESHARE_SALES_DataLayer.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=database.monoinfinity.net;database=TimeshareSalesDB;uid=sa;pwd=1234567890Aa;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer(GetConnectionString());
             }
+        }
+
+        private string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var strConn = config.GetConnectionString("TimeShareSalesDB");
+            return strConn;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
